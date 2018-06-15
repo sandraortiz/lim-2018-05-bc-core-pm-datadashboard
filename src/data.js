b4d5d0660b5a5fbf516feafe06e88c02c7384f97
sandraortiz <rocio12vx@gmail.com>
2018-06-15 14:01:07 -0500

    llamando la data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,16 @@
-const select=document.getElementById('cohort');
 
-fetch('http://127.0.0.1:8080/data/cohorts.json')
+fetch('../data/cohorts.json')
 .then(response => response.json())
 .then(json => {
-  const cohorts = json;
-  console.log(cohorts.length);
-  for (let i = 0; i < cohorts.length; i++) {
+  const cohort = json;
+  console.log(cohort.length);
+  
+  for (let i = 0; i < cohort.length; i++) {
     const options = document.createElement('option');
-    const contentoption = document.createTextNode(cohorts[i].id);
+    const contentoption = document.createTextNode(cohort[i].id);
    options.appendChild(contentoption);
-    console.log(contentoption)
-     console.log(cohort[i].id);
-     select.body.appendChild(option);
+   console.log(cohort[i].id);
+     select.appendChild(options);
     
   }
 })
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const buttonWelcome = document.getElementById("buttonhome");
 const laboratoriapage=document.getElementById("start");
 const welcomePage=document.getElementById("welcomepage");
+const select = document.getElementById('cohort');
 
 buttonWelcome.addEventListener('click', ()=>{
     laboratoriapage.style.display="block";
