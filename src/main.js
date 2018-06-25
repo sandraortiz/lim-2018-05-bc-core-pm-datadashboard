3ee7c7d973a400cceb18111059b70c2ce87420ba
sandraortiz <rocio12vx@gmail.com>
2018-06-24 22:09:40 -0500

    cambios en estilos y llamado de data

--- src/main.js
+++ src/main.js
@@ -1,47 +1,41 @@
-fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
-.then(function(response) {
-  if(response.ok) {
-    response.json()
-.then(function(json) {
-      user = json;
-      cohorts();
-    });
-  } else {
-    console.log('error');
+const welcomePage = document.getElementById('welcome-page');
+const buttonHome = document.getElementById('button-home');
+const menuPage = document.getElementById('menu-page');
+const studentsPage = document.getElementById('students-page');
+const buttonStudents =document.getElementById('button-students');
+buttonHome.addEventListener('click',() => {
+  menuPage.style.display='block';
+  welcomePage.style.display='none';
+  document.body.style.backgroundColor = 'rgba(150, 159, 170, 0.28)';
+  document.body.style.backgroundImage = 'none';
+})
+buttonStudents.addEventListener('click',()=>{
+  menuPage.style.display='none';
+  studentsPage.style.display='block';
+})
+const datafile1 = '../data/cohorts.json'
+const datafile2 = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
+const datafile3 = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+const llamadas = [];
+let cohorts = [];
+let progress = [];
+let users = [];
+llamadas.push(fetch(datafile1));
+llamadas.push(fetch(datafile2));
+llamadas.push(fetch(datafile3));
+Promise.all(llamadas)
+.then(
+  response =>Promise.all(
+    response.map(
+      data => data.text()
+    )
+  )
+ )
+.then(
+  response => {
+    cohorts = JSON.parse(response[0]);
+    progress = JSON.parse(response[1]);
+    users = JSON.parse(response[2]);
   }
-});
-const cohorts = () => {
-  for (let i = 0; i < user.length; i++) {
-    const options = document.createElement('option');
-    const contentoption = document.createTextNode(user[i].name);
-   options.appendChild(contentoption);
-   console.log(user[i].id);
-     select.appendChild(options);
-     }
-const resul = resuls [i];
-     if(resul.id=== "00hJv4mzvqM3D9kBy3dfxoJyFV82") {
-       return resul;
-      }
-
-
-
-}
-
-
-
-const buttonWelcome = document.getElementById("buttonhome");
-const laboratoriapage=document.getElementById("start");
-const welcomePage=document.getElementById("welcomepage");
-const buttonpreadmisio=document.getElementById("students")
-const studentsPge=document.getElementById("studentspage")
-buttonWelcome.addEventListener('click', ()=>{
-    laboratoriapage.style.display="block";
-    welcomePage.style.display="none";
-})
-buttonpreadmisio.addEventListener('click',()=>{
-    studentsPge.style.display="block";
-    laboratoriapage.style.display="none";
-})
-const select = document.getElementById('cohort');
-const students = document.getElementById('user');
+);
 
