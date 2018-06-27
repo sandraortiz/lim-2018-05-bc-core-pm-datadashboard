ab2b1f36f57aedd02ce4da8d2495fe482e5b0947
sandraortiz <rocio12vx@gmail.com>
2018-06-27 11:44:49 -0500

    trabajando los select del los cohorts

--- src/data.js
+++ src/data.js
@@ -1,6 +1,16 @@
-
-const  computeUsersStats = ()=>{
-    
+window.computeUsersStats = (users,progress,courses)=>{
+    users
+    progress
+    courses
 }
 
+window.sortUsers =(users,ordeBy,orderDirection) =>{
 
+}
+
+window.filterUsers=(users,search)=>{
+
+}
+window.processCohortData=(options)=>{
+    
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 const welcomePage = document.getElementById('welcome-page');
 const buttonHome = document.getElementById('button-home');
 const menuPage = document.getElementById('menu-page');
-const studentsPage = document.getElementById('students-page');
-const buttonStudents =document.getElementById('button-students');
+const cohortsPage = document.getElementById('cohorts-page');
+const buttonCohorts =document.getElementById('button-cohorts');
 const students = document.getElementById('user');
+const head = document.getElementById('head')
 buttonHome.addEventListener('click',() => {
   menuPage.style.display='block';
   welcomePage.style.display='none';
+  head.style.display='block';
   document.body.style.backgroundColor = 'rgba(150, 159, 170, 0.28)';
   document.body.style.backgroundImage = 'none';
 })
-buttonStudents.addEventListener('click',()=>{
+buttonCohorts.addEventListener('click',()=>{
   menuPage.style.display='none';
-  studentsPage.style.display='block';
+  cohortsPage.style.display='block';
 })
 const datafile1 = '../data/cohorts.json'
 const datafile2 = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
 const datafile3 = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'
-
-  
 const llamadas = [];
 let cohorts = [];
 let progress = [];
@@ -36,11 +36,26 @@ Promise.all(llamadas)
  )
 .then(
   response => {
-    cohorts = JSON.parse(response[0]);
+    cohorts= (JSON.parse(response[0]));
    users  = JSON.parse(response[1]);
     progress = JSON.parse(response[2]);
-    user(users);
+
   }
    );
-    
   
+   const cohorts = ['lim-2018' , 'lim-2017' , 'aqp-2017' , 'gdl-2018'];
+const sedesSelect = document.getElementById("sedes");
+
+const populateSedesSelect = () => {
+const sedesUnicas=[];
+    cohorts.forEach(cohort=> {
+  const sede = cohort.split('-', 1)[0];
+  console.log(sede);
+  if(sedesUnicas.indexOf(sede) < 0) {
+      sedesUnicas.push(sede);
+  }
+});
+
+}
+
+populateSedesSelect();
